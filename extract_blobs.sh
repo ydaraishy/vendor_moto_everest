ce3aa22596bd2c545c7ff3139ec9e3608d0b667a
dana koch <impatiens@gmail.com>
2012-02-28 10:40:27 +1100

    Initial commit; move from device_moto_everest here.

---
+++ extract_blobs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+mv /tmp/bootanimation.zip proprietary/bootanimation.zip
+mv /tmp/gps.stingray.so proprietary/gps.stingray.so
+mv /tmp/gpsconfig.xml proprietary/gpsconfig.xml
+mv /tmp/chat-ril proprietary/chat-ril
+mv /tmp/location proprietary/location
+mv /tmp/brcm_guci_drv proprietary/brcm_guci_drv
+mv /tmp/pppd-ril.options proprietary/pppd-ril.options
+mv /tmp/gps.conf proprietary/gps.conf
+mv /tmp/bootanimation-encrypted.zip proprietary/bootanimation-encrypted.zip
+mv /tmp/libnvodm_query.so proprietary/libnvodm_query.so
+mv /tmp/libpppd_plugin-ril.so proprietary/libpppd_plugin-ril.so
+mv /tmp/location.cfg proprietary/location.cfg
+mv /tmp/libmoto_ril.so proprietary/libmoto_ril.so
+mv /tmp/pppd-ril proprietary/pppd-ril
+mv /tmp/libnvodm_imager.so proprietary/libnvodm_imager.so
+mv /tmp/camera.stingray.so proprietary/camera.stingray.so
+mv /tmp/akmd2 proprietary/akmd2
---
+++ libpkip_mock.c
@@ -0,0 +1,1 @@
+../../nvidia/wingray/libpkip_mock.c
